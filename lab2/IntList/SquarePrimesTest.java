ec79202624d5fca226e37bd49331793d95219c81
Steven Shang <makisekurisu@sjtu.edu.cn>
2024-03-02 14:45:13 +0800

    lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,42 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesWithAllPrimes() {
+        IntList lst = IntList.of(2, 3, 5, 7);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 25 -> 49", lst.toString());
+        assertTrue(changed);
+    }
+    @Test
+    public void testSquarePrimesWithNoPrimes() {
+        IntList lst = IntList.of(4, 6, 8, 9, 10);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 6 -> 8 -> 9 -> 10", lst.toString());
+        assertFalse(changed);
+    }
+    @Test
+    public void testSquarePrimesWithLargePrime() {
+        IntList lst = IntList.of(10, 15, 997);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("10 -> 15 -> 994009", lst.toString());
+        assertTrue(changed);
+    }
+    @Test
+    public void testSquarePrimesWithSingleElementPrime() {
+        IntList primeList = IntList.of(13);
+        boolean changed = IntListExercises.squarePrimes(primeList);
+        assertEquals("169", primeList.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesWithSingleElementComposite() {
+        IntList compositeList = IntList.of(4);
+        boolean changed = IntListExercises.squarePrimes(compositeList);
+        assertEquals("4", compositeList.toString());
+        assertFalse(changed);
+    }
+
 }
